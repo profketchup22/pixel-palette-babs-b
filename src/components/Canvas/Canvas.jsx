ea092434ebbbcbcab58c6868fe7cdc1dba7f8cd4
profketchup22 <profketchup22@gmail.com>
2026-04-08 19:12:01 -0500

    fixed possible error in canvas pen not accuratly drawing in the right place

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -12,15 +12,25 @@ function Canvas(props) {
     function startDrawing(e) {
 
         // gets the drawing context from the canvas elemnt
-        const ctx = props.canvasRef.current.getContext('2d')
+        const canvas = props.canvasRef.current 
+        const ctx = canvas.getContext('2d')
+
+        // getBoundingClientRect gets the exact precise position of the canvas on the screen
+        // we subtract the canvas top andleft position from the mouse position
+        // gives coordintes relative to canvas wherever it ison the page
+        const rect = canvas.getBoundingClientRect()
+
+        //x and y refrenced ctx.moveTo(x, y)
+        const x = e.clientX - rect.left 
+        const y = e.clientY - rect.top
 
         // begin a new path so each stroke is independent
         ctx.beginPath()
 
         //move to sets the starting point of the line
-        //e.nativeevent.offsetX and offsetY are the x and y coordinates
-        //of where the mouse is relative to the canvas elemnt
-        ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
+        //the x and y coordinates of where the mouse
+        // is relative to the canvas elemnt
+        ctx.moveTo(x, y)
 
         //setIsDrawing to true so onMouseMove knows too start drawing
         setIsDrawing(true)
@@ -31,8 +41,18 @@ function Canvas(props) {
 
         // if mouse button is not held down do nothing
         if (!isDrawing) return
-        // get the drawing context again
-        const ctx = props.canvasRef.current.getContext('2d')
+        // 
+        const canvas = props.canvasRef.current
+        const ctx = canvas.getContext('2d')
+
+        // gets the precise position of the canvas on screen subtracts the margin
+        //outside the canvas to get the exact location youre clicking
+        const rect = canvas.getBoundingClientRect()
+
+        // x y refrenced in // ctx.lineTo(x, y)
+        const x = e.clientX - rect.left
+        const y = e.clientY -rect.top
+
         // sets the color / prop passed fomr CanvasPage
         ctx.strokeStyle = props.color
         //sets the thickness / prop passed from CanvasPage
@@ -41,7 +61,7 @@ function Canvas(props) {
         ctx.lineCap = 'round'
         ctx.lineJoin = 'round'
         // lineTo draws a line from the last position to the current position
-        ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY)
+        ctx.lineTo(x, y)
         // stroke actually makes the line visible on the canvas
         ctx.stroke()
     }
